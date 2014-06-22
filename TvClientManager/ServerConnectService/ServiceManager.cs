135b336a67d25b792e3922ed676e5a3220663bf8
Max <gomon-m@ua.fm>
2014-06-23 00:35:12 +0300

    no message

---
+++ TvClientManager/ServerConnectService/ServiceManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ServerConnectService.ServiceReference;
+
+namespace ServerConnectService
+{
+    public static class ServiceManager
+    {
+        private static TvManagerClientServiceClient clientService;
+
+        public static TvManagerClientServiceClient ClientInstance
+        {
+            get
+            {
+                if (clientService == null)
+                {
+                    clientService = new TvManagerClientServiceClient();
+                }
+
+                return clientService;
+            }
+        }
+    }
+}
